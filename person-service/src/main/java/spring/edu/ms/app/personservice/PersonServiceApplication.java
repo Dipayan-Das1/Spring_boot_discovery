f7688efc81900ca1aee9d05dfbab397696fbfe77
dipayan.das <dipayan.das@oracle.com>
2020-10-23 19:51:08 +0530

    Update person service

--- person-service/src/main/java/spring/edu/ms/app/personservice/PersonServiceApplication.java
+++ person-service/src/main/java/spring/edu/ms/app/personservice/PersonServiceApplication.java
@@ -2,9 +2,11 @@ package spring.edu.ms.app.personservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableSwagger2
 public class PersonServiceApplication {
